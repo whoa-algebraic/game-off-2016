0a0d369cd684a732fb1c30ec15940ea2321967dc
Chris Stevens <munkeyxis@gmail.com>
2016-11-05 19:24:44 -0400

    add basic enemy prefab, add it to scene, tag walls

---
+++ Assets/Scripts/HorizontalAIMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HorizontalAIMovement : MonoBehaviour {
+
+    public float MovementSpeed = 1;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.localPosition += new Vector3(MovementSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Wall") {
+            MovementSpeed *= -1;
+        }
+    }
+}
