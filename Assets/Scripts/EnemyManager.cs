392a5883cfcc7421cf7ca38df2e4bcec5aa6fe31
Peter Colapietro <petercolapietro@gmail.com>
2016-11-14 19:13:25 -0500

    Add namespace

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -3,34 +3,38 @@
 /// <summary>
 /// This is a spawning function I found <see href="https://unity3d.com/learn/tutorials/projects/survival-shooter/more-enemies">here.</see>
 /// </summary>
-public class EnemyManager : MonoBehaviour {
-
-    public PlayerHealth playerHealth;       // Reference to the player's heatlh.
-    public GameObject enemy;                // The enemy prefab to be spawned.
-    public float spawnTime = 3f;            // How long between each spawn.
-    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-
-
-    void Start()
+namespace WhoaAlgebraic
+{
+    public class EnemyManager : MonoBehaviour
     {
-        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
-    }
+
+        public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+        public GameObject enemy;                // The enemy prefab to be spawned.
+        public float spawnTime = 3f;            // How long between each spawn.
+        public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
 
-    void Spawn()
-    {
-        // If the player has no health left...
-        if (playerHealth.currentHealth <= 0f)
+        void Start()
         {
-            // ... exit the function.
-            return;
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
         }
 
-        // Find a random index between zero and one less than the number of spawn points.
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        void Spawn()
+        {
+            // If the player has no health left...
+            if (playerHealth.currentHealth <= 0f)
+            {
+                // ... exit the function.
+                return;
+            }
+
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+            // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+            Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,27 +2,30 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class ScoreManager : MonoBehaviour
+namespace WhoaAlgebraic
 {
-    public static int score;        // The player's score.
-
-
-    Text text;                      // Reference to the Text component.
-
-
-    void Awake()
+    public class ScoreManager : MonoBehaviour
     {
-        // Set up the reference.
-        text = GetComponent<Text>();
-
-        // Reset the score.
-        score = 0;
-    }
+        public static int score;        // The player's score.
 
 
-    void Update()
-    {
-        // Set the displayed text to be the word "Score" followed by the score value.
-        text.text = "Score: " + score;
+        Text text;                      // Reference to the Text component.
+
+
+        void Awake()
+        {
+            // Set up the reference.
+            text = GetComponent<Text>();
+
+            // Reset the score.
+            score = 0;
+        }
+
+
+        void Update()
+        {
+            // Set the displayed text to be the word "Score" followed by the score value.
+            text.text = "Score: " + score;
+        }
     }
 }
