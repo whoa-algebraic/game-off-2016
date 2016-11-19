8e8f92875f2ff63fd2e01dfdf4e56754780bd954
Chris Stevens <munkeyxis@gmail.com>
2016-11-19 17:01:01 -0500

    set Input names and create PlayerActionControls script

---
+++ Assets/Scripts/PlayerActionControls.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActionControls : MonoBehaviour {
+	
+	void Update () {
+		if(Input.GetButtonDown("Action1")) {
+			Debug.Log("Action1 button pressed");
+		}
+
+		if (Input.GetButtonDown("Action2")) {
+			Debug.Log("Action2 button pressed");
+		}
+
+		if (Input.GetButtonDown("Action3")) {
+			Debug.Log("Action3 button pressed");
+		}
+
+		if (Input.GetButtonDown("Action4")) {
+			Debug.Log("Action4 button pressed");
+		}
+	}
+
+}
