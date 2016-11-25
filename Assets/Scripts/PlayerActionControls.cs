dc1d9e13233c6cc51d99b1df924947f97bd877b6
Chris Stevens <munkeyxis@gmail.com>
2016-11-24 22:50:46 -0500

    perform limb action on each button press

--- Assets/Scripts/PlayerActionControls.cs
+++ Assets/Scripts/PlayerActionControls.cs
@@ -4,11 +4,18 @@ public class PlayerActionControls : MonoBehaviour {
 	private GameObject[] limbs;
 
 	void Start() {
+		GameObject bearLimbPrefab = Managers.LimbManager.LimbPrefabs[0];
+
 		limbs = new GameObject[4];
-		limbs[0] = Instantiate(Managers.LimbManager.LimbPrefabs[0]);
-		limbs[0].transform.SetParent(this.transform);
-		limbs[0].transform.localPosition = Vector3.zero;
-		limbs[0].transform.localScale = Vector3.one;
+		
+		limbs[0] = Instantiate(bearLimbPrefab);
+		limbs[1] = Instantiate(bearLimbPrefab);
+		limbs[2] = Instantiate(bearLimbPrefab);
+		limbs[3] = Instantiate(bearLimbPrefab);
+
+		foreach(GameObject limb in limbs) {
+			ConfigureLimbInstance(limb);
+		}
 	}
 
 	void Update () {
@@ -19,15 +26,24 @@ public class PlayerActionControls : MonoBehaviour {
 
 		if (Input.GetButtonDown("Action2")) {
 			Debug.Log("Action2 button pressed");
+			limbs[1].GetComponent<Limb>().Activate();
 		}
 
 		if (Input.GetButtonDown("Action3")) {
 			Debug.Log("Action3 button pressed");
+			limbs[2].GetComponent<Limb>().Activate();
 		}
 
 		if (Input.GetButtonDown("Action4")) {
 			Debug.Log("Action4 button pressed");
+			limbs[3].GetComponent<Limb>().Activate();
 		}
+	}
+
+	private void ConfigureLimbInstance(GameObject limb) {
+		limb.transform.SetParent(this.transform);
+		limb.transform.localPosition = Vector3.zero;
+		limb.transform.localScale = Vector3.one;
 	}
 
 }
